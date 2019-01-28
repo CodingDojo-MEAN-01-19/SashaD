78badda2393a89f161c8622933e0972904a815cc
Aleksandra Dooley <sashadooley@gmail.com>
2019-01-27 17:02:13 -0700

    Landing Page Done

--- 01-javascript/04-advanced/03-fib-plus-nacci.js
+++ 01-javascript/04-advanced/03-fib-plus-nacci.js
@@ -1,21 +1,21 @@
 function fib() {
-    var count = 0;
-    var next_count = 1;
-    var temp;
-    // Some variables here
-    function nacci() {
-      // do something to those variables here
-      temp = next_count;
-      next_count = next_count + count;
-      count = temp;
-      console.log(count);
-    }
-    return nacci
+  var count = 0;
+  var next_count = 1;
+  var temp;
+  // Some variables here
+  function nacci() {
+    // do something to those variables here
+    temp = next_count;
+    next_count = next_count + count; //when it runs the first time count is equal to 0 and next_count is equal to 1
+    count = temp; // here count becomes what next_count was equal to
+    console.log(count);
   }
-  var fibCounter = fib();
-  fibCounter() // should console.log "1"
-  fibCounter() // should console.log "1"˜
-  fibCounter() // should console.log "2"
-  fibCounter() // should console.log "3"
-  fibCounter() // should console.log "5"
-  fibCounter() // should console.log "8"
+  return nacci
+}
+var fibCounter = fib();
+fibCounter() // should console.log "1"
+fibCounter() // should console.log "1"˜
+fibCounter() // should console.log "2"
+fibCounter() // should console.log "3"
+fibCounter() // should console.log "5"
+fibCounter() // should console.log "8"
