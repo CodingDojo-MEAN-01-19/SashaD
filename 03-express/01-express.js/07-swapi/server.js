cb3f5f376a9f5c7a3ddbb9f3106a3b7ddcaaf2c2
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-01 16:33:10 -0700

    Some changes to SWAPI

--- 03-express/01-express.js/07-swapi/server.js
+++ 03-express/01-express.js/07-swapi/server.js
@@ -2,16 +2,52 @@
 
 var express = require("express");
 var app = express();
+
 const axios = require('axios');
 app.use(express.static(__dirname + "/static"));
 
 app.set('views', __dirname + '/views'); 
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response){
-  response.render('index');
+app.get('/', function(req, res){
+  res.render('index');
 });
 
+app.get('/people', function(req, res){
+    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+    axios.get('http://swapi.co/api/people')
+    .then(data => {
+        // log the data before moving on!
+        for(i in data){
+            console.log(data[i].results.name);
+        } 
+        // rather than rendering, just send back the json data!
+        res.json(data);
+    })
+    .catch(error => {
+        // log the error before moving on!
+        console.log(error);
+        res.json(error);
+    })
+});
+app.get('/planets', function(req, res){
+    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+    axios.get('http://swapi.co/api/planets')
+    .then(data => {
+        // log the data before moving on! 
+        console.log('###################################')
+        console.log(data[results]);
+        console.log('###################################')
+        // rather than rendering, just send back the json data!
+        res.json(data);
+    })
+    .catch(error => {
+        // log the error before moving on!
+        console.log('################################################')
+        console.log(error);
+        res.json(error);
+    })
+});
 
 app.listen(8000, function() {
   console.log("listening on port 8000");
