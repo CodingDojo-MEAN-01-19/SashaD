bad78ea332a863d9965b41443efbf87043905173
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-05 11:40:08 -0700

    SWAPI almost done

--- 03-express/01-express.js/07-swapi/server.js
+++ 03-express/01-express.js/07-swapi/server.js
@@ -1,159 +1,135 @@
 // this was part of working up to SWAPI
-
+const session = require('express-session');
 const express = require("express");
 const axios = require('axios');
-const bodyParser = require('body-parser');
+const parser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + "/static"));
+const sessionConfig = {
+    secret: 'ThisIsASecret',
+    resave: false,
+    name: 'session',
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+};
 
-app.set('views', __dirname + '/views'); 
+app.use(session(sessionConfig));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/people', function(req, res){
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+app.get('/people', (req, res) => {
+    // use the axios .get() method - 
+    // provide a url and chain the .then() and .catch() methods
+    //  axios.get(swapiPeopleUrl)
     axios.get('https://swapi.co/api/people?format=json')
     .then(data => {
-        // log the data before moving on!
-        if(data.data.next != null) {
-            data.data.next = data.data.next.replace("https://swapi.co/api/people/?page=", "")[0];
-        }
-        if(data.data.previous != null) {
-            data.data.previous = data.data.previous.replace("https://swapi.co/api/people/?page=", "")[0];
-        }
+        // log the data before moving on! 
+        //console.log("got the data: ", data.data);
+        req.session.next = data.data.next;
+        req.session.previous = data.data.previous;
+        req.session.count = data.data.count;
+        req.session.category = '/people/';
+        //  res.send(data.data);
         // rather than rendering, just send back the json data!
         res.json(data.data);
     })
     .catch(error => {
         // log the error before moving on!
-        console.log(error);
+        console.log('The following errors were generated: ', error);
         res.json(error);
     })
 });
-var returnArray = [];
 
-function getData(count){
-  return axios.get(`https://swapi.co/api/people?page=${count}&format=json`).then(function(response){
-    returnArray = returnArray.concat(response.data);
-    return getData(count +1);
-  }
-  ).catch(function() {
-    return returnArray;
-  }
-)};
-
-app.get('/people/all', function(req,res) {
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    getData(1).then((response) => res.json(response));
-});
-
-app.get('/people/:id', function(req,res) {
-    // use the axios .get() method - provide a url and chain the .then() and .catch() method
-    axios.get('https://swapi.co/api/people?page=' + req.params.id + "&format=json")
-    .then(data => {
-        // data.data.next = data.data.next.replace("https://swapi.co/api", "");
-        // rather than rendering, just send back the json data!
-        if(data.data.next != null) {
-            data.data.next = data.data.next.replace("https://swapi.co/api/people/?page=", "")[0];
-        }
-        if(data.data.previous != null) {
-            data.data.previous = data.data.previous.replace("https://swapi.co/api/people/?page=", "")[0];
-        }
-        res.json(data.data);
-    })
-    .catch(error => {
-        // log the error before moving on!
-        console.log(error);
-        res.json(error);
-    });
-
-});
-app.get('/planets', function(req, res){
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+app.get('/planets', (req, res) =>{
+    // use the axios .get() method - 
+    // provide a url and chain the .then() and .catch() methods
     axios.get('https://swapi.co/api/planets?format=json')
     .then(data => {
-        // log the data before moving on! 
+        // log the data before moving on!
+        //console.log("got the data: ", data.data);
+        req.session.next = data.data.next;
+        req.session.previous = data.data.previous;
+        req.session.count = data.data.count
+        req.session.category = '/planets/'
+        // res.send(data.data);
         // rather than rendering, just send back the json data!
-        if(data.data.next != null) {
-            data.data.next = data.data.next.replace("https://swapi.co/api/planets/?page=", "")[0];
-        }
-        if(data.data.previous != null) {
-            data.data.previous = data.data.previous.replace("https://swapi.co/api/planets/?page=", "")[0];
-        }
         res.json(data.data);
     })
     .catch(error => {
-        // log the error before moving on!
-        console.log('################################################')
-        console.log(error);
+         // log the error before moving on!
+        console.log('The following errors were generated: ', error);
         res.json(error);
     })
 });
 
-var returnPlanetArray = [];
-
-function getPlanetData(count){
-  return axios.get(`https://swapi.co/api/planets?page=${count}&format=json`).then(function(response){
-    returnPlanetArray = returnPlanetArray.concat(response.data);
-    return getPlanetData(count+1);
-  }
-  ).catch(function() {
-    return returnPlanetArray;
-  }
-)};
-
-app.get('/planets/all', function(req,res) {
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    getPlanetData(1).then((response) => res.json(response));
-});
-
-app.get('/planets/:id', function(req,res) {
-    // use the axios .get() method - provide a url and chain the .then() and .catch() method
-
-    axios.get('https://swapi.co/api/planets?page=' + req.params.id + "&format=json")
+app.get('/next', (req, res) => {
+    axios.get(req.session.next)
     .then(data => {
-        // data.data.next = data.data.next.replace("https://swapi.co/api", "");
+        // log the data before moving on!
+        //console.log("got the data: ", data.data);
+        req.session.next = data.data.next;
+        req.session.previous = data.data.previous;
+        // res.send(data.data);
         // rather than rendering, just send back the json data!
-        if(data.data.next != null) {
-            data.data.next = data.data.next.replace("https://swapi.co/api/planets/?page=", "")[0];
-        }
-
-        if(data.data.previous != null) {
-            data.data.previous = data.data.previous.replace("https://swapi.co/api/planets/?page=", "")[0];
-        }
         res.json(data.data);
     })
     .catch(error => {
-        // log the error before moving on!
-        console.log(error);
+         // log the error before moving on!
+        console.log('The following errors were generated: ', error);
         res.json(error);
-    });
-
+    })
 });
 
-var returnPlanetsArray = [];
-
-function getAllPlanets(count){
-  return axios.get(`https://swapi.co/api/planets?page=${count}&format=json`).then(function(response){
-    returnPlanetsArray = returnPlanetsArray.concat(response.data);
-    return getAllPlanets(count +1);
-  }
-  ).catch(function() {
-    return returnPlanetsArray;
-  }
-)};
-
-app.get('/planets/all', function(req,res) {
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    getAllPlanets(1);
-    res.json(returnPlanetsArray);
+app.get('/prev', (req, res) => {
+    axios.get(req.session.previous)
+    .then(data => {
+        // log the data before moving on!
+        //console.log("got the data: ", data.data);
+        req.session.next = data.data.next;
+        req.session.previous = data.data.previous;
+        // res.send(data.data);
+        // rather than rendering, just send back the json data!
+        res.json(data.data);
+    })
+    .catch(error => {
+         // log the error before moving on!
+        console.log('The following errors were generated: ', error);
+        res.json(error);
+    })
 });
 
-app.listen(8000, function() {
-  console.log("listening on port 8000");
+app.get('/all', (req, res) => {
+    var allData = [];
+    for(let i = 1; i <= req.session.count; i++){
+        axios.get('https://swapi.co/api' + req.session.category + i)
+        .then(data => {
+            // log the data before moving on!
+            console.log("got the data: ", data.data);
+            allData.push(data.data);
+            // res.send(data.data);
+            // rather than rendering, just send back the json data!
+            res.json(data.data);
+        })
+        .catch(error => {
+             // log the error before moving on!
+            console.log('The following errors were generated: ', error);
+            res.json(error);
+        })
+    }
+    return allData;
+});
+
+
+app.listen(8080, function() {
+  console.log("listening on port 8080");
 })
