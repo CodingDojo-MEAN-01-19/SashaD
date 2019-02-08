c2b6b7a0c240e776502ee315a23d4fd60d1da387
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-07 21:44:00 -0700

    More chat changes

--- 03-express/02-socket.io/04-group-chat/server.js
+++ 03-express/02-socket.io/04-group-chat/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var parser = require('body-parser');
+var session = require('express-session');
 const server = app.listen(1999);
 const io = require('socket.io')(server);
 const path = require('path');
@@ -10,17 +11,36 @@ app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views'); 
 app.set('view engine', 'ejs');
 
+app.set(session({
+  secret: 'ThisIsASecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 60000}
+}));
+
 app.get('/', function(req, res){
   res.render('index');
 });
-//on is method to listen for event
-io.on('connection', socket => {
-  socket.on('name_submit', function(data){
-    console.log("We got here!")
-    console.log(data);
-    socket.broadcast.emit('new_name', data);
-  });
-  //now handle the form!
+
+var people = [];
+io.sockets.on('connection', function(socket) {
+  console.log("connected");
+    //people.push(socket);
+    //console.log(socket)
+    //console.log("we got here")
+
+    socket.on('new_person', function(data){
+      console.log("got the new person! " + data);
+      people.push(data)
+      socket.broadcast.emit('person_sent', data);
+      io.emit('person_sent', data);
+    });
+    socket.on('disconnect', function(data) {
+      console.log('Got disconnect!');
+
+      var i = people.indexOf(data);
+      people.splice(i, 1);
+   });
 });
 
 
