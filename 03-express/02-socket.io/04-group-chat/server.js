f071647bde5b0ff64c645c13a72111d06ff46435
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-09 17:30:55 -0700

    The broken chat

--- 03-express/02-socket.io/04-group-chat/server.js
+++ 03-express/02-socket.io/04-group-chat/server.js
@@ -1,10 +1,11 @@
 var express = require("express");
-var app = express();
-var parser = require('body-parser');
-var session = require('express-session');
-const server = app.listen(1999);
-const io = require('socket.io')(server);
 const path = require('path');
+var session = require('express-session');
+var parser = require('body-parser');
+const io = require('socket.io'),
+  connect = require('connect');
+
+var app = express().connect().use(connect.static('public')).listen(3000);
 
 
 app.use(express.static(__dirname + "/static"));
@@ -18,30 +19,32 @@ app.set(session({
   cookie: {maxAge: 60000}
 }));
 
+let all_messages = [];
+var users = [];
+
 app.get('/', function(req, res){
   res.render('index');
 });
 
-var people = [];
-io.sockets.on('connection', function(socket) {
-  console.log("connected");
-    //people.push(socket);
-    //console.log(socket)
-    //console.log("we got here")
-
-    socket.on('new_person', function(data){
-      console.log("got the new person! " + data);
-      people.push(data)
-      socket.broadcast.emit('person_sent', data);
-      io.emit('person_sent', data);
+io.on('connection', function(socket){
+  socket.on('login', function(data){
+      users.push = data.person;
+      console.log(data.person);
+      socket.emit('init_board', {all_messages: all_messages});
+      socket.once('disconnect', function (){
+        var pos = users.indexOf(name);
+        if (pos >= 0){
+          users.splice(pos, 1);
+        };
+      });
+      socket.on('send', function(data){
+        console.log(data.msg)
+        all_messages.push(session.person + ": " + data.msg)
+        io.emit('msg_update', {msg: session.username + ": " + data.msg});
     });
-    socket.on('disconnect', function(data) {
-      console.log('Got disconnect!');
+  });
 
-      var i = people.indexOf(data);
-      people.splice(i, 1);
-   });
+  
 });
 
-
-console.log("Listening on port " + JSON.stringify(server['_connectionKey']));
+console.log("Listening on port " + JSON.stringify(server['_connectionKey']))
