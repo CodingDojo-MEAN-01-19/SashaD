1da4ec0507a08e0a14371e8470b8f36363ae87a7
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-09 23:58:53 -0700

    The solution that doesn't work

--- 03-express/02-socket.io/04-group-chat/server.js
+++ 03-express/02-socket.io/04-group-chat/server.js
@@ -1,51 +1,23 @@
-var express = require("express");
-var app = express();
-var parser = require('body-parser');
-var session = require('express-session');
-const server = app.listen(1999);
-const io = require('socket.io')(server);
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const bodyParser = require('body-parser');
+const app = express();
 
-
-app.use(express.static(__dirname + "/static"));
-app.set('views', __dirname + '/views'); 
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.set(session({
-  secret: 'ThisIsASecret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {maxAge: 60000}
-}));
-
-let all_messages = [];
-var users = [];
-
-app.get('/', function(req, res){
-  res.render('index');
+// tell the express app to listen on port 1999
+const server = app.listen(1999, function() {
+  console.log("listening on port 1999");
 });
 
-io.on('connection', function(socket){
-  socket.on('login', function(data){
-    console.log(data);
-    users.push(data);
-    console.log(data.person);
-    let current_person = data.person;
-    socket.emit('current_person', {current_person: current_person});
-    socket.emit('init_board', {all_messages: all_messages});
-    socket.on('send', function(data){
-      console.log(data.msg);
-      console.log(current_person);
-      all_messages.push(current_person + ": " + data.msg)
-      io.emit('msg_update', {msg: current_person + ": " + data.msg});
-    });
-    socket.on('disconnect', function (person){  
-      var pos = users.indexOf(person);
-      if (pos >= 0){
-        users.splice(pos, 1);
-      };
-    });
-  });
-});
+// load the routes file, pass app and server into it (for handling url visits and events)
+require('./routes/index.js')(app, server);
 
-console.log("Listening on port " + JSON.stringify(server['_connectionKey']))
+
+//The supposed answer.... 
+// It isn't loading properly
