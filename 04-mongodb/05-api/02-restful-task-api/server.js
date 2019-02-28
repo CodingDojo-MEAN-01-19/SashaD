3e108180183e16351b9516ccde6f8c9d4f724abd
Aleksandra Dooley <sashadooley@gmail.com>
2019-02-27 19:24:16 -0700

    Finished Hello Angular

--- 04-mongodb/05-api/02-restful-task-api/server.js
+++ 04-mongodb/05-api/02-restful-task-api/server.js
@@ -18,14 +18,16 @@ app.use(session({
   }));
 
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './client/static')));
+app.use(express.static(path.join(__dirname, './public/dist/public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './client/views'));
 
 require('./server/config/database.js');
 
-app.use(require('./server/config/routes'));
+// routes
+app.use('/tasks', require('./server/config/routes/task.routes'));
+app.use(express.static(__dirname + '/public/dist/public'));;
 
 app.listen(port, () => console.log(`Listening on port ${ port}`));
 //this is the task api assignment
